483debc65831cc770df5890ef0ddc2a0bd9767d4
java <java@DESKTOP-RFL91OU>
2019-10-17 11:25:13 +0900

    2019.10.17. ch01 프로젝트 등록

---
+++ ch01/src/sub1/HelloJava.java
@@ -0,0 +1,14 @@
+package sub1;
+
+/*
+ * 날짜: 2019.10.10.
+ * 이름: 황재호
+ * 내용: 자바 Hello world 출력하기
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java!");
+	}
+}
